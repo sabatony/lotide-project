342a3347e18d7dd254df2070db0327f5dfb63b74
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-05-23 23:06:29 -0700

    add assertEquals

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: ✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ❌ ${actual} !== ${expected}`);
+  }
+
+};
+assertEqual(1, 1);
