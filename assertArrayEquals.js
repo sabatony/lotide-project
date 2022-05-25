dd0497ec0841c37655847bff4c92de73fbbf5369
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-05-25 16:08:56 -0700

    add assert array

---
+++ assertArrayEquals.js
@@ -0,0 +1,29 @@
+const eqArrays = function (arr1, arr2){
+  if(arr1.length !== arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+
+  
+};
+
+const assertEqualArray = function(actual, expected){
+   
+   let arr1 = [];
+   let arr2 = [];
+   
+  if(actual === eqArrays(arr1, arr2)){
+    console.log(`Assertion passed: ✅ ${actual} === ${expected}`);
+  } else if (actual !== eqArrays(arr1, arr2)) {
+    console.log(`Assertion failed: ❌ ${actual} !== ${expected}`);
+  }
+
+
+};
+
+assertEqualArray(eqArrays([1, 2, 3,], ['1', '2', '3',]), false);
