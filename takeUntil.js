64e1f21ae067649126b8f96efb3c703f991f8173
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-03 16:51:02 -0700

    add takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,22 @@
+const takeUntil = function(array, callback) {
+  results = [];
+  for(let element of array){
+    if(!callback(element)){
+      results.push(element)
+    } else{
+      break;
+    }
+    
+    
+  }
+  return results
+  
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
