4cc74f4c54301414924f9e1503ec0c700072bc7b
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-10 13:16:30 -0700

    refactor tail with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail')
 
-const words = ["Yo Yo", 'lighthouse', 'labs'];
-(tail(words));
-assertEqual(words.length, 3);
+
+const assert = require('assert')
+const tail = require('../tail');
+
+describe('#tail', function (){
+  it("should return ['lighthouse', 'labs'] for ['yo yo', 'lighthouse', 'labs']", function(){
+    assert.deepStrictEqual(tail(["Yo Yo", 'lighthouse', 'labs']), ['lighthouse', 'labs'])
+  });
+  it('should return 3 for original array length', function(){
+    const words = ["Yo Yo", 'lighthouse', 'labs'];
+    tail(words);
+    assert.deepStrictEqual(words.length, 3)
+  })
+});
