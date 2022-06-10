afc8a4137eaf5024538ff25a8c577874e9b97b30
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-06-09 20:32:15 -0700

    refactor with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,16 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head')
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+const assert = require('assert')
+const head = require('../head');
+
+describe('#head', () => {
+  it('should return 1 for [1, 2, 3]', () => {
+     assert.deepStrictEqual(head([1, 2, 3]), 1)
+  });
+  it("returns '5' for ['5']", () => {
+    assert.deepStrictEqual(head(['5']), '5');
+  });
+  it('should return "hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepStrictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello')
+  });
+});
